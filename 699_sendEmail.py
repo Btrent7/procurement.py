6500a9f7bf811a6c44e423716c81ceaf215b5694
Brandon Trent <185147333+Btrent7@users.noreply.github.com>
2025-04-22 09:14:17 -0500

    _sendEmail.py

--- 699_sendEmail.py
+++ 699_sendEmail.py
@@ -1,27 +1,9 @@
-# import smtplib
-# from email.mime.text import MIMEText
-
-# subject = "Part Number"
-# body = "Hello! Test."
-# sender = "email@gmail.com"
-# recipients = "email@reliablesprinkler.com"
-# password = "pass_word1"
-
-# def send_email (subject, body, sender, recipients, password):
-#     msg = MIMEText(body)
-#     msg = ['Subject'] = subject
-#     msg = ['From'] = sender
-#     msg = ['To'] = recipients
-#     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
-#         smtp_server.login(sender, password)
-#         smtp_server.sendmail(sender, recipients, msg.as_string())
-#     print("Message Sent!") 
-
 import base64
 from email.mime.text import MIMEText
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from requests import HTTPError 
+
 SCOPES = [ "https://www.googleapis.com/auth/gmail.send"]
 flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
 creds = flow.run_local_server(port=0)
