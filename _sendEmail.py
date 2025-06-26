80fe0e6673ece53434a1e61b62d45209354306a2
Brandon Trent <185147333+Btrent7@users.noreply.github.com>
2025-06-26 10:47:31 -0500

    Gmail API Email Sender
    Outline for API Gmail connection and automated email

--- _sendEmail.py
+++ _sendEmail.py
@@ -4,7 +4,7 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from requests import HTTPError 
 
-# GMAIL creditials
+# GMAIL creditials 
 SCOPES = [ "https://www.googleapis.com/auth/gmail.send"]
 flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
 creds = flow.run_local_server(port=0)
